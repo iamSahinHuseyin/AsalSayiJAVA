9d5d1e953a8f0a710ac306f5da6252f5fe73a09b
sahin <sahinhuseyinn0307@gmal.com>
2024-05-25 18:42:11 +0300

    Fisished Commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,28 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    // ilk önce 1000 e kadar olan sayıları ekrana yazdıralım
-    public static void ekranaYaz(){
-        for(int i=2;i<=1000;i++){
-            System.out.println(i);
+    //asal sayi bulmak için metodumuz
+    //asal sayı kendisi ve 1 dışında böleni olmayan sayıdır.
+    public static boolean asalSayi(int sayi){
+        for(int i=2;i<sayi;i++){
+            //eğer sayının böleni varsa asal değildir
+            //bu yüzden FALSE değeri döner.
+            if(sayi % i == 0){
+                return false;
+            }
         }
+        return true;
     }
     public static void main(String[] args) {
-
-        ekranaYaz();
+        //1000 e kadar olan sayıları ekrana yazdıralım
+        for(int i=2;i<1000;i++){
+            //asal sayi ise TRUE döndürmesi gereklidir.
+            if(asalSayi(i)){
+                System.out.println(i + " = Asal sayıdır.");
+            }
+            else{
+                System.out.println(i + " = Asal sayı DEĞİLDİR.");
+            }
+        }
     }
 }
