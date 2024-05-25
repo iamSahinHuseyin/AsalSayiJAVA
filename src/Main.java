e43a3ab86cf1824c79bae725171ae33fa3ce9ef5
sahin <sahinhuseyinn0307@gmal.com>
2024-05-25 18:34:46 +0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    // ilk önce 1000 e kadar olan sayıları ekrana yazdıralım
+    public static void ekranaYaz(){
+        for(int i=2;i<=1000;i++){
+            System.out.println(i);
+        }
+    }
+    public static void main(String[] args) {
+
+        ekranaYaz();
+    }
+}
